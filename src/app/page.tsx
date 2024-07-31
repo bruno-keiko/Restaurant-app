3383366c6ef7b66fea6a9c31045d702834a3dc58
Begench <gurbanmammedovbegench07@gmail.com>
2024-07-31 14:58:36 +0500

    fix(button): deleted non-existing component's import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Button from "@/components/Button";
 
 export default function Home() {
   return (
@@ -26,7 +25,6 @@ export default function Home() {
               priority
             />
           </a>{" "}
-          <Button />
         </div>
       </div>
 
