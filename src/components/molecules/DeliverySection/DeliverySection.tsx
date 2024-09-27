316d1dc577fa3e0b286e32644d8267fca54e0bfe
Florian Cherubin <floriancherubin@gmail.com>
2024-09-27 10:32:21 +0200

    add of DeliverySection component

---
+++ src/components/molecules/DeliverySection/DeliverySection.tsx
@@ -0,0 +1,38 @@
+import DeliveryOrderCard from "@/components/atoms/deliveryOrderCard";
+import Button from "@/components/atoms/button/Button";
+import Image from "next/image";
+import styles from "./DeliverySection.module.scss";
+
+const DeliverySection: React.FC = ({}) => {
+  return (
+    <div className={styles["delivery-section"]}>
+      <h2 className={styles["delivery-section__sectionTitle"]}>Delivery</h2>
+      <div className={styles["delivery-section__content"]}>
+        <div className={styles["delivery-section__left"]}>
+          <h2 className={styles["delivery-section__left__title"]}>
+            A Moments Of <span>Delivered On Right Time & Place</span>
+          </h2>
+          <p>
+            Food Khan is a restaurant, bar and coffee roastery located on a busy
+            corner site in Farringdon's Exmouth Market. With glazed frontage on
+            two sides of the building, overlooking the market and a bustling
+            London inteon.
+          </p>
+          <div className={styles["delivery-section__delivery-oreder-card"]}>
+            <DeliveryOrderCard title="Delivery Order" tel="+880 1630 225 015" />
+            <Button children="Order Now" size="small" />
+          </div>
+        </div>
+        <Image
+          src="/images/image_delivery_section.png"
+          alt={"food delivery"}
+          width="447"
+          height="330"
+          // className={styles["delivery-section__image"]}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DeliverySection;
