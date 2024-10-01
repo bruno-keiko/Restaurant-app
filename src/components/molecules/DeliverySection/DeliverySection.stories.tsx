dd355facefb538fc70c016fe13dc75078a553974
Florian Cherubin <floriancherubin@gmail.com>
2024-10-01 14:07:29 +0200

    modification on story and h2 title font size

--- src/components/molecules/DeliverySection/DeliverySection.stories.tsx
+++ src/components/molecules/DeliverySection/DeliverySection.stories.tsx
@@ -1,16 +1,13 @@
 import { Meta } from "@storybook/react";
 import DeliverySection from "./DeliverySection";
 
+export const Default = () => {
+  return <DeliverySection />;
+};
+
 const deliverySection: Meta<typeof DeliverySection> = {
   title: "Molecules/DeliverySection",
   component: DeliverySection,
-  tags: ["autodocs"],
-  excludeStories: /.*Data$/,
-  decorators: [
-    (Story) => {
-      return <Story />;
-    },
-  ],
 };
 
 export default deliverySection;
