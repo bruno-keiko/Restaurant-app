cfd7c524fd93d75f03d26f0e8463c5d60b73cd3c
syedaCodes <syedaa.codes@gmail.com>
2024-10-09 19:53:54 +0530

    create template for similar pages with background image and header content

--- src/components/pages/ItemsPage/ItemsPage.tsx
+++ src/components/pages/ItemsPage/ItemsPage.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import styles from "./ItemsPage.module.scss";
+import SecondaryHeaderPages from "@/components/templates/SecondaryHeaderPages";
 
 interface ItemsPageProps {}
 
 const ItemsPage: React.FC<ItemsPageProps> = () => {
   return (
     <div className={styles["items-page"]}>
-      <section className={styles["secondary-header"]}>
-        <div className={styles["secondary-header--textContent"]}>
-          <h2>Items</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius
-            mod tempor incididunt ut labore et dolore magna.
-          </p>
-        </div>
-      </section>
-      <h3>It&apos;s the food and groceries you love, delivered</h3>
-      <section></section>
-      <section></section>
+      <SecondaryHeaderPages
+        backgroundImg="/images/itemsheadbg.png"
+        title="items"
+        textContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius mod tempor incididunt ut labore et dolore magna."
+      >
+        <h3>It&apos;s the food and groceries you love, delivered</h3>
+        <section></section>
+        <section></section>
+      </SecondaryHeaderPages>
     </div>
   );
 };
---
+++ src/components/templates/SecondaryHeaderPages/SecondaryHeaderPages.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta } from "@storybook/react";
+import SecondaryHeaderPages from "./SecondaryHeaderPages";
+
+const secondaryHeaderPages: Meta<typeof SecondaryHeaderPages> = {
+  title: "Pages/SecondaryHeaderPages",
+  component: SecondaryHeaderPages,
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => {
+      return (
+        <div>
+          <Story />
+        </div>
+      );
+    },
+  ],
+};
+
+export const Default = {
+  args: {
+    title: "ITEMS",
+    backgroundImg: "/images/itemsheadbg.png",
+    textContent:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius mod tempor incididunt ut labore et dolore magna.",
+  },
+};
+
+export default secondaryHeaderPages;
