76298aed144c40588e4eb882a2a6334923d4ab14
w0wayush <ayupandey82@gmail.com>
2024-10-09 22:35:14 +0530

    Done with login section

---
+++ src/components/molecules/LoginComponent/LoginSection.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import Header from "../Header";
+import Image from "next/image";
+import BannerImage from "@/../../public/images/banner.png";
+import CopyrightInfo from "@/components/atoms/CopyrightInfo";
+import Link from "next/link";
+import styles from "./LoginSection.module.scss";
+import { MdOutlineAccessTimeFilled } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+
+const LoginSection: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <Header />
+      <div className={styles.banner}>
+        <Image
+          src={BannerImage}
+          alt="Banner image"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+        <div className={styles.overlay} />
+        <h1>My Account</h1>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.forms}>
+          <div className={styles["login-form"]}>
+            <h2>Login</h2>
+            <form>
+              <div className={styles["form-group"]}>
+                <label>Username or email address *</label>
+                <input type="text" placeholder="Enter your email" required />
+              </div>
+              <div className={styles["form-group"]}>
+                <label>Password *</label>
+                <input type="password" placeholder="Enter password" required />
+              </div>
+              <button type="submit">Log in</button>
+              <div className={styles.rememberMe}>
+                <input type="checkbox" id="remember" />
+                <label htmlFor="remember">Remember me</label>
+              </div>
+              <a href="#" className={styles["lost-password"]}>
+                Lost your password?
+              </a>
+            </form>
+            <div className={styles["new-user"]}>
+              New user? <Link href="/signup">sign up here...</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles["branch-section"]}>
+        <h2>Our Branch</h2>
+        <div className={styles.branches}>
+          {["Robert Food", "Mark A. Reed Food", "Karie K. Hill Food"].map(
+            (branch, index) => (
+              <div key={index} className={styles.branch}>
+                <h3>{branch}</h3>
+                <p>
+                  {
+                    [
+                      "1986 Hilltop DriveBorger, TX 79007",
+                      "4877 Rose AvenueNew Orleans, LA 70112",
+                      "1509 Peaceful LaneCleveland, OH 44115",
+                    ][index]
+                  }
+                </p>
+                <p>
+                  {" "}
+                  <MdOutlineAccessTimeFilled size={20} />
+                  7:30 AM - 9:30 PM
+                </p>
+                <p>
+                  <FaPhoneAlt size={20} />
+                  +880 1630-225015
+                </p>
+                <a href="#">Click to View Google Map</a>
+              </div>
+            ),
+          )}
+        </div>
+      </div>
+      <CopyrightInfo text="Copyright © 2024 | Shaheen Uddin Ahmad" />
+    </div>
+  );
+};
+
+export default LoginSection;
