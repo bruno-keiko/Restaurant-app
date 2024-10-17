923b2c8e684dd1a2c9384d857001830e1339423b
Begench <gurbanmammedovbegench07@gmail.com>
2024-10-17 16:51:04 +0500

    feat: Vercel speed insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./global.scss";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children} <Analytics />
+        {children} <Analytics /> <SpeedInsights />
       </body>
     </html>
   );
